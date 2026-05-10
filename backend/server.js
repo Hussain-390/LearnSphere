9d6a8ca87e20b00e901eb22180cfac9c181e4886
Hussain-390 <sk.mhussain2006@gmail.com>
2026-05-10 10:51:44 +0530

    Day 1: LearnSphere initial setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => res.send('EduFlow API running'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
